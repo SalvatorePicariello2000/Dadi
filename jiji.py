009e617ee4e6283c879e019e2793cdbec7fbaa51
SalvatorePicariello <salvatorepica00@gmail.com>
2018-11-12 12:47:55 +0000

    ewfgergerw

---
+++ jiji.py
@@ -0,0 +1,12 @@
+import random
+Prova=1000000
+i=0
+lista=[0,0,0,0,0,0]
+def dado ():
+	return random.randrange (1,7)
+for i in range(Prova):
+	risultato=dado()
+	lista[risultato-1]+=1
+	#print (risultato, end=' ')
+
+print (lista)
